7d5521e8c507f958e1f4582d384adffb6824b0e0
68000a <68000a@gmx.net>
2026-06-29 11:48:10 +0200

    KiKA Mediathek - 2.0.8 Das API der KiKA Mediathek wurde abgeschaltet

--- code/plugin.video.kikamediathek/default.py
+++ code/plugin.video.kikamediathek/default.py
@@ -1,4 +1,13 @@
-﻿# -*- coding: utf-8 -*-
-import libkika
+# -*- coding: utf-8 -*-
+import xbmc
+import xbmcgui
+import libmediathek3 as libMediathek
+import platform
 
-libkika.list()
+libMediathek.endOfDirectory()
+dialog = xbmcgui.Dialog()
+title = 'KiKA Mediathek'
+text = 'Im Juni 2026 wurde das API der KiKA Mediathek abgeschaltet. Die von der ARD bereitgestellten KiKA Inhalte sind über die ARD-Mediathek abrufbar, die vom ZDF bereitgestellten KiKA Inhalte sind über die ZDF-Mediathek oder über ZDF-Tivi abrufbar.'
+text = text + ' [Py' + platform.python_version() + ']'
+dialog.ok(title, text)
+xbmc.executebuiltin('XBMC.ActivateWindow(Home)')
